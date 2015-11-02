3fbe525ba45e8c5c43fc24458e65041e831814a1
guipsamora <gui.psamora@gmail.com>
2015-11-02 13:40:02 -0200

    Insert team name and it is the excel file

--- javascript.js
+++ javascript.js
@@ -1,100 +1,72 @@
 $(document).ready(function() {
 
-var start = 0;
-var elapsed = 0;
-var results = [];
+  var start = 0;
+  var elapsed = 0;
+  var results = [];
+  var team;
+  var hours = 0;
+  var minutes = 0;
+  var seconds = 0;
+  var time = 0;
 
- $("button").click(function(){
-
+  $("button").click(function(){
    if (start === 0){
       start = new Date();
    } else {
-      // 
-      elapsed = new Date() - start;
-      var minutes = (elapsed / 1000)/ 60;
-      minutes = minutes.toFixed(2); 
-
+      getTime();
       // pushes member and minutes since timer started to array results
-      results.push([this.className, minutes]);
-      console.log(minutes);
+      results.push([this.className, time]);
       console.log(results);
    }
  });
 
- $(".export").click(function(){
-    exportData()
- });
+// export array results to excel xlsx file and clean the results array
+  $(".export").click(function(){
+    exportData();
+    clearResults();
+  });
 
 // Keyboard shortcuts from 1 to 5 to represent the team members and space to represent silence
- $("button").keypress(function (e) {  
-  if (e.which === 49) {
-      elapsed = new Date() - start;
-      var minutes = (elapsed / 1000)/ 60
-      minutes = minutes.toFixed(2); 
-      // pushes member and minutes since timer started to array results
-      results.push(["Member A", minutes]);
-  } else if(e.which === 50){
-      elapsed = new Date() - start;
-      var minutes = (elapsed / 1000)/ 60
-      minutes = minutes.toFixed(2); 
-      // pushes member and minutes since timer started to array results
-      results.push(["Member B", minutes]);
-  } else if(e.which === 51){
-      elapsed = new Date() - start;
-      var minutes = (elapsed / 1000)/ 60
-      minutes = minutes.toFixed(2); 
-      // pushes member and minutes since timer started to array results
-      results.push(["Member C", minutes]);
-  } else if(e.which === 52){
-      elapsed = new Date() - start;
-      var minutes = (elapsed / 1000)/ 60
-      minutes = minutes.toFixed(2); 
-      // pushes member and minutes since timer started to array results
-      results.push(["Member D", minutes]);
-  } else if(e.which === 53){
-      elapsed = new Date() - start;
-      var minutes = (elapsed / 1000)/ 60
-      minutes = minutes.toFixed(2); 
-      // pushes member and minutes since timer started to array results
-      results.push(["Member E", minutes]);
-  } else if(e.which === 32){
-      elapsed = new Date() - start;
-      var minutes = (elapsed / 1000)/ 60
-      minutes = minutes.toFixed(2); 
-      // pushes member and minutes since timer started to array results
-      results.push(["Silence", minutes]);
-  }
-});
-
- // $(".button1").click(function(){
- //   // this.member = member;
- //   var elapsed = new Date() - start;
- //   var minutes = (elapsed / 1000)/ 60;
- //   console.log(minutes)
- // });
+  $("button").keypress(function (e) {  
+    if (e.which === 49) {
+        getTime();
+        results.push(["Member A", minutes]);
+    } else if(e.which === 50){
+        getTime();
+        results.push(["Member B", minutes]);
+    } else if(e.which === 51){
+        getTime();
+        results.push(["Member C", minutes]);
+    } else if(e.which === 52){
+        getTime();
+        results.push(["Member D", minutes]);
+    } else if(e.which === 53){
+        getTime();
+        results.push(["Member E", minutes]);
+    } else if(e.which === 32){
+        getTime();
+        results.push(["Silence", minutes]);
+    }
+  });
 
 // function that exports array results to excel .xlsx
-    function exportData() {
-        alasql("SELECT * INTO XLSX('speakers.xlsx',{headers:true}) FROM ? ",[results]);
-    }
+  function exportData() {
+      team = $("#team").val();
+      alasql("SELECT * INTO XLSX(\'"+ team + ".xlsx\',{headers:true}) FROM ? ",[results]);
+  }
 
-//  function timeConversation(member){
-//    this.member = member;
-//    var start = new Date();
-//    var elapsed = new Date() - start;
-//    var minutes = (elapsed * 1000)/ 60
-// }
+// clear the results array
+  function clearResults(){
+    results = [];
+  }
+
+// 
+  function getTime(){
+    elapsed = new Date() - start;
+    hours = "00";
+    minutes = Math.floor(elapsed / 60000);
+    seconds = ((elapsed % 60000) / 1000).toFixed(0);
+    time = hours + ":" + (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  }
 
 });
-
-
-// identify team member or silence or parallel
-
-// create function that begins timer
-
-// create function that stops timer and store result to array or object
-
-// make keyboard shorcuts
-
-// format minutes from number to Date
-
