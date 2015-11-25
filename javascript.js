e324b02a7db816909aee0ea125e8be203823617a
guipsamora <gui.psamora@gmail.com>
2015-11-25 16:02:44 -0200

    Improved

--- javascript.js
+++ javascript.js
@@ -222,17 +222,18 @@ $("#rewind").click(function(){
 });
 
 (function localFileVideoPlayerInit(win) {
-    var URL = win.URL || win.webkitURL,
-        displayMessage = (function displayMessageInit() {
+    var URL = win.URL || win.webkitURL;
+    
+    var displayMessage = (function displayMessageInit() {
             var node = document.querySelector('#message');
 
             return function displayMessage(message, isError) {
                 node.innerHTML = message;
                 node.className = isError ? 'error' : 'info';
             };
-        }()),
+        }());
 
-    playSelectedFile = function playSelectedFileInit(event) {
+    var playSelectedFile = function playSelectedFileInit(event) {
             var file = this.files[0];
             var type = file.type;
             var videoNode = document.querySelector('#video1');
@@ -252,8 +253,9 @@ $("#rewind").click(function(){
             var fileURL = URL.createObjectURL(file);
 
             videoNode.src = fileURL;
-        },
-        inputNode = document.querySelector('#videoSource');
+        };
+
+    var inputNode = document.querySelector('#videoSource');
 
     if (!URL) {
         displayMessage('Your browser is not ' + '<a href="http://caniuse.com/bloburls">supported</a>!', true);
